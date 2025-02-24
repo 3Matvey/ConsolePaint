43c9956f7610df988eaa3bf4c47818dac8a88789
3Matvey <andrianov8m@gmail.com>
2025-02-25 01:16:38 +0300

    fixes

--- ConsolePaint/Program.cs
+++ ConsolePaint/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using ConsolePaint;
+using ConsolePaint.Shapes;
 class Program
 {
     static void Main()
@@ -73,45 +74,6 @@ class Program
     }
 }
 
-class Canvas(int width, int height)
-{
-    private readonly char[,] grid = new char[height, width];
 
-    public void Clear()
-    {
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                grid[i, j] = ' ';
-            }
-        }
-    }
-
-    public void Display()
-    {
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                Console.Write(grid[i, j]);
-            }
-            Console.WriteLine();
-        }
-    }
-
-    public void Draw(Shape shape)
-    {
-        shape.Draw(this);
-    }
-
-    public void SetPixel(int x, int y, char symbol)
-    {
-        if (x >= 0 && x < width && y >= 0 && y < height)
-        {
-            grid[y, x] = symbol;
-        }
-    }
-}
 
 
