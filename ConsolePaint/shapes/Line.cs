87ae46d5d0c05066f8c21ee4578849f22e8501ca
3Matvey <andrianov8m@gmail.com>
2025-02-25 16:16:02 +0300

    base structure

---
+++ ConsolePaint/shapes/Line.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsolePaint.Shapes
+{
+    class Line(int x1, int y1, int x2, int y2, char symbol) : Shape
+    {
+        public override void Draw(Canvas canvas)
+        {
+            // Простое рисование горизонтальных или вертикальных линий
+            if (x1 == x2)  // вертикальная линия
+            {
+                for (int y = y1; y <= y2; y++)
+                {
+                    canvas.SetPixel(x1, y, symbol);
+                }
+            }
+            else if (y1 == y2)  // горизонтальная линия
+            {
+                for (int x = x1; x <= x2; x++)
+                {
+                    canvas.SetPixel(x, y1, symbol);
+                }
+            }
+        }
+    }
+}
---
+++ ConsolePaint/shapes/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePaint.Shapes
+{
+    class Rectangle(int x1, int y1, int x2, int y2, char symbol) : Shape
+    {
+        public override void Draw(Canvas canvas)
+        {
+            // Рисуем верхнюю и нижнюю горизонтальные линии
+            for (int x = x1; x <= x2; x++)
+            {
+                canvas.SetPixel(x, y1, symbol);
+                canvas.SetPixel(x, y2, symbol);
+            }
+
+            // Рисуем боковые вертикальные линии
+            for (int y = y1; y <= y2; y++)
+            {
+                canvas.SetPixel(x1, y, symbol);
+                canvas.SetPixel(x2, y, symbol);
+            }
+        }
+    }
+}
